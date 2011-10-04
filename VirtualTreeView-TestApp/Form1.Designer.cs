329fde7a99a08151b78145d80811305882bfb217
Dmitry Maslakov <dmaslakov@gmail.com>
2011-10-04 23:59:00 +0700

    VirtualTreeView-TestApp extended to support different procedures to reproduce issues; one sch procedure (the draft) added for massl load flickering

--- VirtualTreeView-TestApp/Form1.Designer.cs
+++ VirtualTreeView-TestApp/Form1.Designer.cs
@@ -29,16 +29,19 @@
         private void InitializeComponent()
         {
 			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.numLoadDelay = new System.Windows.Forms.NumericUpDown();
 			this.tbNodeStatus = new System.Windows.Forms.TextBox();
 			this.btRemoveSelectedNode = new System.Windows.Forms.Button();
 			this.btAddChildNode = new System.Windows.Forms.Button();
 			this.btAddRootNode = new System.Windows.Forms.Button();
 			this.xbAutoPromote = new System.Windows.Forms.CheckBox();
 			this.xbAutoVirtual = new System.Windows.Forms.CheckBox();
-			this.numLoadDelay = new System.Windows.Forms.NumericUpDown();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
 			this.vtv = new MartianGuiControls.VirtualTreeView();
+			this.cbReproduceIssue = new System.Windows.Forms.ComboBox();
+			this.btClearTree = new System.Windows.Forms.Button();
+			this.btReproduceIssue = new System.Windows.Forms.Button();
 			this.splitContainer1.Panel1.SuspendLayout();
 			this.splitContainer1.Panel2.SuspendLayout();
 			this.splitContainer1.SuspendLayout();
@@ -54,6 +57,9 @@
 			// 
 			// splitContainer1.Panel1
 			// 
+			this.splitContainer1.Panel1.Controls.Add(this.btReproduceIssue);
+			this.splitContainer1.Panel1.Controls.Add(this.btClearTree);
+			this.splitContainer1.Panel1.Controls.Add(this.cbReproduceIssue);
 			this.splitContainer1.Panel1.Controls.Add(this.label2);
 			this.splitContainer1.Panel1.Controls.Add(this.label1);
 			this.splitContainer1.Panel1.Controls.Add(this.numLoadDelay);
@@ -69,73 +75,31 @@
 			// 
 			this.splitContainer1.Panel2.Controls.Add(this.vtv);
 			this.splitContainer1.Panel2MinSize = 50;
-			this.splitContainer1.Size = new System.Drawing.Size(550, 316);
+			this.splitContainer1.Size = new System.Drawing.Size(550, 400);
 			this.splitContainer1.SplitterDistance = 162;
 			this.splitContainer1.TabIndex = 1;
 			// 
-			// tbNodeStatus
+			// label2
 			// 
-			this.tbNodeStatus.Location = new System.Drawing.Point(50, 270);
-			this.tbNodeStatus.Name = "tbNodeStatus";
-			this.tbNodeStatus.ReadOnly = true;
-			this.tbNodeStatus.Size = new System.Drawing.Size(92, 20);
-			this.tbNodeStatus.TabIndex = 5;
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(9, 245);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(59, 13);
+			this.label2.TabIndex = 8;
+			this.label2.Text = "Node type:";
 			// 
-			// btRemoveSelectedNode
+			// label1
 			// 
-			this.btRemoveSelectedNode.Location = new System.Drawing.Point(12, 192);
-			this.btRemoveSelectedNode.Name = "btRemoveSelectedNode";
-			this.btRemoveSelectedNode.Size = new System.Drawing.Size(130, 23);
-			this.btRemoveSelectedNode.TabIndex = 4;
-			this.btRemoveSelectedNode.Text = "Remove selected node";
-			this.btRemoveSelectedNode.UseVisualStyleBackColor = true;
-			this.btRemoveSelectedNode.Click += new System.EventHandler(this.btRemoveSelectedNode_Click);
-			// 
-			// btAddChildNode
-			// 
-			this.btAddChildNode.Location = new System.Drawing.Point(12, 163);
-			this.btAddChildNode.Name = "btAddChildNode";
-			this.btAddChildNode.Size = new System.Drawing.Size(130, 23);
-			this.btAddChildNode.TabIndex = 3;
-			this.btAddChildNode.Text = "Add child node";
-			this.btAddChildNode.UseVisualStyleBackColor = true;
-			this.btAddChildNode.Click += new System.EventHandler(this.btAddChildNode_Click);
-			// 
-			// btAddRootNode
-			// 
-			this.btAddRootNode.Location = new System.Drawing.Point(13, 134);
-			this.btAddRootNode.Name = "btAddRootNode";
-			this.btAddRootNode.Size = new System.Drawing.Size(129, 23);
-			this.btAddRootNode.TabIndex = 2;
-			this.btAddRootNode.Text = "Add root node";
-			this.btAddRootNode.UseVisualStyleBackColor = true;
-			this.btAddRootNode.Click += new System.EventHandler(this.btAddRootNode_Click);
-			// 
-			// xbAutoPromote
-			// 
-			this.xbAutoPromote.AutoSize = true;
-			this.xbAutoPromote.Location = new System.Drawing.Point(13, 50);
-			this.xbAutoPromote.Name = "xbAutoPromote";
-			this.xbAutoPromote.Size = new System.Drawing.Size(129, 17);
-			this.xbAutoPromote.TabIndex = 1;
-			this.xbAutoPromote.Text = "Promote to real nodes";
-			this.xbAutoPromote.UseVisualStyleBackColor = true;
-			this.xbAutoPromote.CheckedChanged += new System.EventHandler(this.xbAutoPromote_CheckedChanged);
-			// 
-			// xbAutoVirtual
-			// 
-			this.xbAutoVirtual.AutoSize = true;
-			this.xbAutoVirtual.Location = new System.Drawing.Point(13, 26);
-			this.xbAutoVirtual.Name = "xbAutoVirtual";
-			this.xbAutoVirtual.Size = new System.Drawing.Size(129, 17);
-			this.xbAutoVirtual.TabIndex = 0;
-			this.xbAutoVirtual.Text = "New nodes are virtual";
-			this.xbAutoVirtual.UseVisualStyleBackColor = true;
-			this.xbAutoVirtual.CheckedChanged += new System.EventHandler(this.xbAutoVirtual_CheckedChanged);
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(9, 55);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(102, 13);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "Delay (milliseconds):";
 			// 
 			// numLoadDelay
 			// 
-			this.numLoadDelay.Location = new System.Drawing.Point(50, 86);
+			this.numLoadDelay.Location = new System.Drawing.Point(49, 71);
 			this.numLoadDelay.Maximum = new decimal(new int[] {
             10000,
             0,
@@ -146,41 +110,112 @@
 			this.numLoadDelay.TabIndex = 6;
 			this.numLoadDelay.ValueChanged += new System.EventHandler(this.numLoadDelay_ValueChanged);
 			// 
-			// label1
+			// tbNodeStatus
 			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(12, 70);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(102, 13);
-			this.label1.TabIndex = 7;
-			this.label1.Text = "Delay (milliseconds):";
+			this.tbNodeStatus.Location = new System.Drawing.Point(12, 261);
+			this.tbNodeStatus.Name = "tbNodeStatus";
+			this.tbNodeStatus.ReadOnly = true;
+			this.tbNodeStatus.Size = new System.Drawing.Size(129, 20);
+			this.tbNodeStatus.TabIndex = 5;
 			// 
-			// label2
+			// btRemoveSelectedNode
 			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(12, 254);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(59, 13);
-			this.label2.TabIndex = 8;
-			this.label2.Text = "Node type:";
+			this.btRemoveSelectedNode.Location = new System.Drawing.Point(12, 165);
+			this.btRemoveSelectedNode.Name = "btRemoveSelectedNode";
+			this.btRemoveSelectedNode.Size = new System.Drawing.Size(130, 23);
+			this.btRemoveSelectedNode.TabIndex = 4;
+			this.btRemoveSelectedNode.Text = "Remove selected node";
+			this.btRemoveSelectedNode.UseVisualStyleBackColor = true;
+			this.btRemoveSelectedNode.Click += new System.EventHandler(this.btRemoveSelectedNode_Click);
+			// 
+			// btAddChildNode
+			// 
+			this.btAddChildNode.Location = new System.Drawing.Point(12, 136);
+			this.btAddChildNode.Name = "btAddChildNode";
+			this.btAddChildNode.Size = new System.Drawing.Size(130, 23);
+			this.btAddChildNode.TabIndex = 3;
+			this.btAddChildNode.Text = "Add child node";
+			this.btAddChildNode.UseVisualStyleBackColor = true;
+			this.btAddChildNode.Click += new System.EventHandler(this.btAddChildNode_Click);
+			// 
+			// btAddRootNode
+			// 
+			this.btAddRootNode.Location = new System.Drawing.Point(12, 107);
+			this.btAddRootNode.Name = "btAddRootNode";
+			this.btAddRootNode.Size = new System.Drawing.Size(129, 23);
+			this.btAddRootNode.TabIndex = 2;
+			this.btAddRootNode.Text = "Add root node";
+			this.btAddRootNode.UseVisualStyleBackColor = true;
+			this.btAddRootNode.Click += new System.EventHandler(this.btAddRootNode_Click);
+			// 
+			// xbAutoPromote
+			// 
+			this.xbAutoPromote.AutoSize = true;
+			this.xbAutoPromote.Location = new System.Drawing.Point(12, 35);
+			this.xbAutoPromote.Name = "xbAutoPromote";
+			this.xbAutoPromote.Size = new System.Drawing.Size(129, 17);
+			this.xbAutoPromote.TabIndex = 1;
+			this.xbAutoPromote.Text = "Promote to real nodes";
+			this.xbAutoPromote.UseVisualStyleBackColor = true;
+			this.xbAutoPromote.CheckedChanged += new System.EventHandler(this.xbAutoPromote_CheckedChanged);
+			// 
+			// xbAutoVirtual
+			// 
+			this.xbAutoVirtual.AutoSize = true;
+			this.xbAutoVirtual.Location = new System.Drawing.Point(12, 12);
+			this.xbAutoVirtual.Name = "xbAutoVirtual";
+			this.xbAutoVirtual.Size = new System.Drawing.Size(129, 17);
+			this.xbAutoVirtual.TabIndex = 0;
+			this.xbAutoVirtual.Text = "New nodes are virtual";
+			this.xbAutoVirtual.UseVisualStyleBackColor = true;
+			this.xbAutoVirtual.CheckedChanged += new System.EventHandler(this.xbAutoVirtual_CheckedChanged);
 			// 
 			// vtv
 			// 
 			this.vtv.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.vtv.Location = new System.Drawing.Point(0, 0);
 			this.vtv.Name = "vtv";
-			this.vtv.Size = new System.Drawing.Size(384, 316);
+			this.vtv.Size = new System.Drawing.Size(384, 400);
 			this.vtv.TabIndex = 0;
 			this.vtv.CreateRealChildren += new MartianGuiControls.VirtualTreeViewCreateChildrenHandler(this.virtualTreeView1_CreateRealChildren);
 			this.vtv.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.virtualTreeView1_BeforeExpand);
 			this.vtv.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.virtualTreeView1_AfterSelect);
 			this.vtv.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.virtualTreeView1_NodeMouseClick);
 			// 
+			// cbReproduceIssue
+			// 
+			this.cbReproduceIssue.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbReproduceIssue.FormattingEnabled = true;
+			this.cbReproduceIssue.Location = new System.Drawing.Point(12, 336);
+			this.cbReproduceIssue.Name = "cbReproduceIssue";
+			this.cbReproduceIssue.Size = new System.Drawing.Size(130, 21);
+			this.cbReproduceIssue.TabIndex = 9;
+			// 
+			// btClearTree
+			// 
+			this.btClearTree.Location = new System.Drawing.Point(12, 195);
+			this.btClearTree.Name = "btClearTree";
+			this.btClearTree.Size = new System.Drawing.Size(129, 23);
+			this.btClearTree.TabIndex = 11;
+			this.btClearTree.Text = "Clear tree";
+			this.btClearTree.UseVisualStyleBackColor = true;
+			this.btClearTree.Click += new System.EventHandler(this.btClearTree_Click);
+			// 
+			// btReproduceIssue
+			// 
+			this.btReproduceIssue.Location = new System.Drawing.Point(12, 307);
+			this.btReproduceIssue.Name = "btReproduceIssue";
+			this.btReproduceIssue.Size = new System.Drawing.Size(129, 23);
+			this.btReproduceIssue.TabIndex = 12;
+			this.btReproduceIssue.Text = "Reproduce issue:";
+			this.btReproduceIssue.UseVisualStyleBackColor = true;
+			this.btReproduceIssue.Click += new System.EventHandler(this.btReproduceIssue_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(550, 316);
+			this.ClientSize = new System.Drawing.Size(550, 400);
 			this.Controls.Add(this.splitContainer1);
 			this.MinimumSize = new System.Drawing.Size(300, 350);
 			this.Name = "Form1";
@@ -207,6 +242,9 @@
 		private System.Windows.Forms.NumericUpDown numLoadDelay;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox cbReproduceIssue;
+		private System.Windows.Forms.Button btClearTree;
+		private System.Windows.Forms.Button btReproduceIssue;
     }
 }
 
--- VirtualTreeView-TestApp/Form1.cs
+++ VirtualTreeView-TestApp/Form1.cs
@@ -3,6 +3,9 @@
 using System;
 using System.Windows.Forms;
 using MartianGuiControls;
+using System.ComponentModel;
+using System.Reflection;
+using System.Collections.Generic;
 
 namespace VirtualTreeViewTestApp
 {
@@ -16,6 +19,33 @@ namespace VirtualTreeViewTestApp
 			vtv.NewNodesVirtual = xbAutoVirtual.Checked;
 			vtv.PromoteToRealNode = xbAutoPromote.Checked;
 			vtv.ChildrenLoadDelay = Convert.ToInt32(numLoadDelay.Value);
+
+			// fill helpers for how-reproduce-issue
+			LoadIssueItems();
+		}
+
+		private Dictionary<int, Delegate> _issueHandlers = new Dictionary<int, Delegate>();
+
+		private delegate void ReproduceIssueHandler();
+
+		private void LoadIssueItems()
+		{
+			MethodInfo[] miList = this.GetType().GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+			foreach (MethodInfo mi in miList)
+			{
+				if (mi.Name.StartsWith("Issue_"))
+				{
+					object[] attrList = mi.GetCustomAttributes(typeof(DescriptionAttribute), true);
+					if (attrList != null && attrList.Length > 0)
+					{
+						foreach (DescriptionAttribute attr in attrList)
+						{
+							int idx = cbReproduceIssue.Items.Add(attr.Description);
+							_issueHandlers.Add(idx, Delegate.CreateDelegate(typeof(ReproduceIssueHandler), this, mi));
+						}
+					}
+				}
+			}
 		}
 
 		void virtualTreeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
@@ -79,5 +109,68 @@ namespace VirtualTreeViewTestApp
 			vtv.ChildrenLoadDelay = Convert.ToInt32(((NumericUpDown)sender).Value);
 		}
 
+		private void btClearTree_Click(object sender, EventArgs e)
+		{
+			vtv.Nodes.Clear();
+		}
+
+		private void btReproduceIssue_Click(object sender, EventArgs e)
+		{
+			int idx = cbReproduceIssue.SelectedIndex;
+			if (idx >= 0 && _issueHandlers.ContainsKey(idx))
+			{
+				_issueHandlers[idx].DynamicInvoke();
+			}
+		}
+
+		#region Issues reproduction
+		// All methods must be:
+		// - static
+		// - name starts with Issue_
+		// - take single argument of type VirtualTreeView
+		// - marked with Description attribute
+
+		[Description("flicker: mass load")]
+		private void Issue_MassLoad_Flicker()
+		{
+			if (_flickerTimer != null)
+			{
+				MessageBox.Show("Flicker test is already running.");
+				return;
+			}
+			_flickerTimer = new Timer();
+			_flickerTimer.Tick += new EventHandler(_flickerTimer_Tick);
+			_flickerTimer.Interval = 100; // 10 times per second
+			_flickerTimer.Tag = 0;
+			_flickerTimer.Start();
+		}
+
+		private const int _flickerCount = 10000;
+		private Timer _flickerTimer;
+
+		void _flickerTimer_Tick(object sender, EventArgs e)
+		{
+			int cnt = (int)_flickerTimer.Tag;
+			int max = Math.Min(cnt + 100, _flickerCount);
+
+			for (; cnt < max; ++cnt)
+			{
+				vtv.Nodes.Add("mass load #" + cnt.ToString());
+			}
+
+			if (cnt < _flickerCount)
+			{
+				_flickerTimer.Tag = cnt;
+			}
+			else
+			{
+				// destroy timer
+				_flickerTimer.Dispose();
+				_flickerTimer = null;
+			}
+		}
+
+
+		#endregion
 	}
 }
